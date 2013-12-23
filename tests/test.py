989ecb2de00da872aca014a582f37b18f10a1e88
ajjackson <a.j.jackson@bath.ac.uk>
2013-12-23 19:55:45 +0000

    another test

--- tests/test.py
+++ tests/test.py
@@ -2,11 +2,19 @@
 
 import unittest
 from compound_electroneg import compound_electroneg
+import smact_core
 
 class TestSequenceFunctions(unittest.TestCase):
 
     def setUp(self):
         pass
+
+    def test_Element_class_Pt(self):
+        Pt = smact_core.Element('Pt')
+        self.assertEqual(Pt.name,'Platinum')
+        self.assertEqual(Pt.ionpot,8.9588)
+
+#TODO(AJJ): Implement warnings in a testable way. Have a Pu testcase.
     
     def test_compound_eneg_brass(self):
         self.assertEqual(compound_electroneg(
