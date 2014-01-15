118de7d91d9e1a0d4554c21d3766d51c6ea879be
Keith Butler <k.t.butler@bath.ac.uk>
2014-01-15 16:28:27 +0000

    Updated distorter and builder

--- smact_builder.py
+++ smact_builder.py
@@ -23,10 +23,16 @@
 from ase.lattice.spacegroup import crystal
 
 
-def cubic_perovskite(species,cell_par=[6,6,6,90,90,90]):
+def cubic_perovskite(species,cell_par=[6,6,6,90,90,90],repititions=[1,1,1]):
 	 system = crystal((species), 
 	 basis=[(0,0,0), (0.5, 0.5, 0.5), (0.5, 0.5, 0)],
-         spacegroup=221, cellpar=[6,6,6,90,90,90])
+         spacegroup=221, size = repititions, cellpar=cell_par)
 	
          return system
 
+def spinel(species,cell_par=[8,8,8,90,90,90],repititions=[1,1,1]):
+	 system = crystal((species),
+	 basis=[(0.0, 0.0, 0.0),(0.625, 0.625, 0.625),(0.3873, 0.3873, 0.3873)],
+	 spacegroup=227,size=repititions,cellpar=cell_par)
+
+         return system
