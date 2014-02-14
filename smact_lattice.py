ddd961cab32e9dec1c359236151f3f44d178c26a
Keith Butler <k.t.butler@bath.ac.uk>
2014-02-14 10:40:54 +0000

    Updated files for generating the list of compounds satisfying charge neutrality

--- smact_lattice.py
+++ smact_lattice.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python 
+
+################################################################################
+#  Copyright Keith T. Butler, Adam J. Jackson (2013)                           #
+#                                                                              #
+# This file is part of SMACT: smact_lattice.py is free software: you can       #
+# redistribute it and/or modify it under the terms of the GNU General Public   #
+# License as published by the Free Software Foundation, either version 3 of    #
+# the License, or (at your option) any later version.                          #
+# This program is distributed in the hope that it will be useful, but WITHOUT  #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        #
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for    #
+# more details.                                                                #
+# You should have received a copy of the GNU General Public License along with #
+# this program.  If not, see <http://www.gnu.org/licenses/>.                   #
+################################################################################
+
 import numpy as np
 
 class Lattice(object):
-      """A class of objects to hold the structure stoiciometry"""
+      """A class of objects to hold the structure stoichiometry"""
 
       def __init__(self, sites, site_ratios, site_oxidations):
 	self.sites = sites
@@ -10,16 +26,43 @@ class Lattice(object):
 	self.site_oxidations = site_oxidations
 #------------------------------------------------------------------------------------
 def check_lattice_charges(charges, site_elements, sites):
+      """
+      This function checks the sum of the charges on the lattice sites in the crystal
+      Additional explanation if needed
+
+      Args:
+          charges: array of the charge contributions of each lattice site
+          site_elements: All possible elements which satisfy charge neutrality 
+          sites: list, the current set of elements under inspection
+
+      Returns:
+          input site_elements array with appended neutral sites (???)
+
+      """
 # Check if all sub-lattice charges sum to zero, append to list if true
-	if np.sum(charges) == 0:
+      if np.sum(charges) == 0:
+          site_elements.append(sites)
 # Need to use two lists, new contains the present composition; site_elements is the 
 # list of all news found.
-	    site_elements.append(sites)
+#
+# 
 
-        return site_elements
+      return site_elements
 #------------------------------------------------------------------------------------
-def possible_compositions(crystal):
-    """Seach for the elements which satisfy the possible oxidations and chrge neutrlity"""
+def possible_compositions(crystal, elements):
+    """Seach for the elements which satisfy the possible oxidations and charge neutrality.
+
+    Args:
+        crystal: A Lattice object defining the crystal class
+        elements: Dictionary of elements and their allowable oxidation states
+    Uses:
+  	atom : a list of possible atoms on each site of the lattice
+
+    Returns:
+        list/array/dict of int/float/string of something useful (???)
+
+    """
+
 # Initialise the array atom, containing possible elements for each sub lattice
     atom = []
 # Initialise the array, site_elements, containing compositions found
@@ -28,7 +71,6 @@ def possible_compositions(crystal):
     while i < len(crystal.sites):
         atom.append(possible_elements(elements, crystal.site_oxidations[i]))
         i = i + 1
-
 # I could not think of an elegant way to generalise this. For now it loops through the possible
 # lattices until it reaches the number, then it goes no further. Therefore, many of the loops
 # here are redundant. 
@@ -36,41 +78,51 @@ def possible_compositions(crystal):
 # we multiply these by oxidation number to get sub-lattice charge
     i = 0
     for site_1 in atom[0]:
-        breaker = 0
         charges = np.zeros(shape=(len(atom)))
-        charges[0] = elements[site_1] * crystal.site_ratios[0]
-	if len(atom) == 1:
-	    sites = [site_1]
+# Look up the charge of 'site_1' multiply by the multiplicity of that site to get charge
+        charges[0] = int(elements[site_1]) * crystal.site_ratios[0]
+	if len(atom) == 1: #Are ther more sites to check?
+	    sites = [site_1] 
 	    site_elements = check_lattice_charges(charges, site_elements, sites)
 	if 2 <= len(atom):       #Are ther more sites to check?
       	    for site_2 in atom[1]:
-                charges[1] = elements[site_2] * crystal.site_ratios[1]
+                charges[1] = int(elements[site_2]) * crystal.site_ratios[1]
 		if len(atom) == 2:    #If we have sampled all sites test the charge
 	            sites = [site_1, site_2]
 		    site_elements = check_lattice_charges(charges, site_elements, sites)
 	        if 3 <= len(atom):    #Are ther more sites to check?
                     for site_3 in atom[2]:
-                        charges[2] = elements[site_3] * crystal.site_ratios[2]
+                        charges[2] = int(elements[site_3]) * crystal.site_ratios[2]
 	                if len(atom) == 3: #If we have sampled all sites test the charge
 	                    sites = [site_1, site_2, site_3]
 		            site_elements = check_lattice_charges(charges, site_elements, sites)
 		        if 4 <= len(atom): #Are ther more sites to check?
                             for site_4 in atom[3]:
-                                charges[3] = elements[site_4] * crystal.site_ratios[3]
+                                charges[3] = int(elements[site_4]) * crystal.site_ratios[3]
 	                        if len(atom) == 4: 
 	                            sites = [site_1, site_2, site_3, site_4]
 		                    site_elements = check_lattice_charges(charges, site_elements, sites)
 			    if 5 <= len(atom):
 			    	for site_5 in atom[4]:
-				    charges[4] = elements[site_5] * crystal_site_ratios[4] 
+				    charges[4] = int(elements[site_5]) * crystal_site_ratios[4] 
 	                            if len(atom) == 5:
 	                                sites = [site_1, site_2, site_3, site_4]
 		                        site_elements = check_lattice_charges(charges, site_elements, sites)
 
     return site_elements
+
 #------------------------------------------------------------------------------------
 def possible_elements(elements, oxidations):
-    """Possible atoms to occupy a site"""
+    """Identify possible atoms to occupy a site
+    
+    Args:
+        elements:
+        oxidations: 
+
+    Returns: 
+        Array of atoms
+
+    """
     atoms = []
     for element in elements:
         for ox_state_a in oxidations:
@@ -78,19 +130,4 @@ def possible_elements(elements, oxidations):
         	    atoms.append(element)
     return atoms
 #------------------------------------------------------------------------------------
-
-elements ={'Cu' : 2, 'Pb' : 2, 'Ti' : 4, 'I' : -1, 'O' : -2, 'Nb' : 4, 'Cl' : -1, 'Sn' : 1, 'S' : -2}
-
-
-#perovskite = Lattice(["A","B","C"],[1,1,3],[[1,2],[2,3,4],[-1,-2]])
-#mixed_perovskite = Lattice(["A","B","C","D"],[0.5,0.5,1,3],[[1,2,3],[1,2,3],[2,3,4],[-1,-2]])
-
-# Perovskite A B X_3
-#perovskite_compositions = possible_compositions(perovskite)
-# Mixed A site perovskite A_0.5 M_0.5 B X_3
-#mixed_perovskite_compositions = possible_compositions(mixed_perovskite)
-
-#print perovskite_compositions
-#print mixed_perovskite_compositions
-
 
--- test/Example_build_compound_library.py
+++ test/Example_build_compound_library.py
@@ -1,11 +1,27 @@
 from smact_lattice import *
-from CellBuilder import *
+from smact_builder import *
 from ase.io import *
 
-elements ={'Cu' : 2, 'Pb' : 2, 'Ti' : 4, 'I' : -1, 'O' : -2, 'Nb' : 4, 'Cl' : -1, 'Sn' : 1, 'S' : -2}
+
+# Generate a dictionary elements, form the dataset oxidationstates.data
+# Dictionary contains elements and their oxidation states
+# Reduce the regions of the periodic table to visit, by using search_space
+search_space = {'Pb','Ti','O-','Cs','Sn','F-','Cl','I-','Ca','Sr'}
+
+elements = {}
+f = open('oxidationstates.data','r')
+lines = f.readlines()
+f.close()
+for line in lines:
+    inp = line.split()
+    for item in search_space:
+        if inp[0].startswith(item): 
+    	    key = inp[0]
+    	    elements[key] = inp[1]
+
 
 perovskite = Lattice(["A","B","C"],[1,1,3],[[1,2],[2,3,4],[-1,-2]])
-perovskite_compositions = possible_compositions(perovskite)
+perovskite_compositions = possible_compositions(perovskite, elements)
 
 i = 0
 for composition in perovskite_compositions:
