d291c0ef826b77561c78d9a04a8a1fdbfdbb1ea3
Keith Butler <k.t.butler@bath.ac.uk>
2014-05-08 14:41:24 +0100

    Uncle Joe's great purge.

--- OldFiles/test/KNbO3.py
+++
@@ -1,9 +0,0 @@
-import ase
-import smact_builder as builder
-import numpy as np
-from ase.io import *
-
-# Build the input
-test_case = builder.cubic_perovskite(['K','Nb','O'],[4.07, 4.07, 4.07, 90, 90, 90])
-
-write('KNbO3_Cubic.cif',test_case)
