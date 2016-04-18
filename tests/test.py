397bf2ae926e2143a7f84af105161e22c5f8a748
Adam J. Jackson <a.j.jackson@physics.org>
2016-04-18 15:17:50 +0100

    Remove old builder tests

--- tests/test.py
+++ tests/test.py
@@ -108,39 +108,5 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertEqual((ZnS.sites[0].position[0]), 2./3.)
 
 
-'''
-    def test_compound_library(self):
-        """A compound test, this covers builder, oxidation_data and the possible_compositions functions"""
-        this_directory = os.path.dirname(__file__)
-        if this_directory:
-            this_directory = this_directory + '/'
-        smact_directory = this_directory + '../smact/'
-
-# Generate a dictionary elements, form the dataset oxidationstates.data
-# Dictionary contains elements and their oxidation states
-# Reduce the regions of the periodic table to visit, by using search_space
-        search_space = {'Ti','O-','Sn','F-','C','Sr','Mg','Cu','Li','S','Si','Ge'}
-# Get the list of possible constituant elements
-        elements = {}
-        f = open(path.join(smact.data_directory, 'oxidationstates.data'), 'r')
-        lines = f.readlines()
-        f.close()
-        for line in lines:
-            inp = line.split()
-            for item in search_space:
-                if inp[0].startswith(item):
-                    key = inp[0]
-                    elements[key] = inp[1]
-
-# Generate list of compositions which satisfy charge neutrality
-        perovskite = smact.lattice.Lattice(["A","B","C"],[1,1,3],[[1,2],[2,3,4],[-1,-2]])
-        perovskite_compositions = smact.lattice.possible_compositions(perovskite, elements)
-        self.assertEqual(perovskite_compositions[3][2],'C-2')
-        self.assertEqual(perovskite_compositions[12][2],'S-2')
-        self.assertEqual(perovskite_compositions[20][1],'Ge4')
-        self.assertEqual(perovskite_compositions[32][0],'S2')
-#--------------------------------------------------------------------------------------------------------
-'''
-
 if __name__ == '__main__':
     unittest.main()
