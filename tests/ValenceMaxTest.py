5404e94e31434d263c39703180fbbfe6ef257a4d
Daniel Davies <dd264@bath.ac.uk>
2016-04-29 14:02:06 +0100

    Update VBM test

--- tests/ValenceMaxTest.py
+++ tests/ValenceMaxTest.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
+import smact
 from numpy import product,sqrt
-"""get_eig function not yet working"""
 from smact.data import get_covalent
 from scipy.constants import hbar, m_e
-"""Get element names"""
+
+
+
+# Get element info
 An =raw_input("Enter Anion Symbol: ") 
 Cat =raw_input("Enter Cation Symbol: ")
-eig_an = 10.27 
-eig_cat = 4.14
+eig_an = smact.Element(An).eig
+eig_cat = smact.Element(Cat).eig
 
-"""calculations"""
+# Calculations
 V2 = (2.16*(hbar)**2)/m_e*(((get_covalent(An)**-9))+((get_covalent(Cat))**-9)**2)
 
 V3 =  (eig_cat - eig_an)/2 
