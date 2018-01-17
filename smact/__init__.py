c07430f1bc5ccf1e0b326a065efdd3c684da118c
Daniel Davies <dd264@bath.ac.uk>
2018-01-17 13:45:22 +0000

    Add d block metals list

--- smact/__init__.py
+++ smact/__init__.py
@@ -381,7 +381,7 @@ def neutral_ratios(oxidations, stoichs=False, threshold=5):
                                                         threshold=threshold)]
     return (len(allowed_ratios) > 0, allowed_ratios)
 
-# List of metals for which we have Pauling electronegativity values
+# List of metals
 metals = ['Li','Be','Na','Mg','Al','K','Ca','Sc','Ti','V','Cr','Mn','Fe','Co','Ni',
 'Cu','Zn','Ga','Rb','Sr','Y','Zr','Nb','Mo','Tc','Ru','Rh','Pd','Ag','Cd','In','Sn',
 'Cs','Ba','La','Ce', 'Pr','Nd','Sm','Eu','Gd','Tb','Dy','Ho','Er','Tm','Yb',
@@ -393,3 +393,8 @@ metals = ['Li','Be','Na','Mg','Al','K','Ca','Sc','Ti','V','Cr','Mn','Fe','Co','N
 anions = ["N", "P", "As", "Sb",
            "O", "S", "Se", "Te",
            "F", "Cl", "Br", "I"]
+
+# List of d-block metals 
+d_block = ['Sc','Ti','V','Cr','Mn','Fe','Co','Ni','Cu','Zn',
+           'Y','Zr','Nb','Mo','Tc','Ru','Rh','Pd','Ag','Cd',
+           'La','Hf','Ta','W','Re','Os','Ir','Pt','Au','Hg']
