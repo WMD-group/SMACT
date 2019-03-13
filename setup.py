51ebf823eb8fabba016f1e0280c87813b4f11414
Daniel Davies <dan@Daniels-MacBook-Pro.local>
2019-03-13 15:24:11 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 __author__ = "Daniel W. Davies"
-__copyright__ = "Copyright Adam J. Jackson, Daniel W. Davies (2013)"
-__version__ = "1.2"
+__copyright__ = "Copyright Daniel W. Davies, Adam J. Jackson, Keith T. Butler (2019)"
+__version__ = "2.0"
 __maintainer__ = "Daniel W. Davies"
-__email__ = "d.w.davies@bath.ac.uk"
-__date__ = "Feb 6 2017"
+__email__ = "d.davies16@imperial.ac.uk"
+__date__ = "Mar 13 2019"
 
 from setuptools import setup
 import os
@@ -16,18 +16,18 @@ module_dir = os.path.dirname(os.path.abspath(__file__))
 if __name__ == "__main__":
     setup(
         name='SMACT',
-        version='1.2',
+        version='2.0',
         description='Semiconducting Materials by Analogy and Chemical Theory',
         long_description=open(os.path.join(module_dir, 'README.md')).read(),
         url='https://github.com/WMD-group/SMACT',
         author='Daniel W. Davies',
-        author_email='d.w.davies@bath.ac.uk',
+        author_email='d.davies16@imperial.ac.uk',
         license='GNU General Public License (GPL) v3',
         packages=['smact','smact.tests'],
         package_data={'smact': ['data/*.txt','data/*.csv']},
         zip_safe=False,
         test_suite='smact.tests.test',
-        install_requires=['scipy','numpy','spglib'],
+        install_requires=['scipy','numpy','spglib', 'pymatgen', 'ase'],
         classifiers=['Programming Language :: Python',
                      'Development Status :: 5 - Production/Stable',
                      'Intended Audience :: Science/Research',
