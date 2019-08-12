0ea609356f806f1083387cc6dea4287d30992067
Alex Moriarty <amoriarty14@gmail.com>
2019-08-12 16:42:21 +0100

    Setup test suite in tests module __init__

---
+++ smact/tests/__init__.py
@@ -0,0 +1,13 @@
+import unittest
+
+import smact.tests.test
+from smact.tests.test_structure import StructureDBTest, StructureTest
+
+if __name__ == "__main__":
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+    suite.addTests(loader.loadTestsFromModule(smact.tests.test))
+    suite.addTests(loader.loadTestsFromTestCase(StructureTest))
+    suite.addTests(loader.loadTestsFromTestCase(StructureDBTest))
+    runner = unittest.TextTestRunner()
+    result = runner.run(suite)
