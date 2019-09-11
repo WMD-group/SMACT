2c7aea094cf55f0c99af8ebfa8b0470918ca6396
Alex Moriarty <amoriarty14@gmail.com>
2019-09-11 14:15:50 +0100

    Docstring update: utilities.py

--- smact/structure_prediction/utilities.py
+++ smact/structure_prediction/utilities.py
@@ -5,7 +5,22 @@ from typing import Tuple
 
 
 def parse_spec(species: str) -> Tuple[str, int]:
-    """Parse a species string into its element and charge."""
+    """Parse a species string into its element and charge.
+
+    Args:
+        species (str): String representation of a species in
+            the format {element}{absolute_charge}{sign}.
+
+    Returns:
+        A tuple of (element, signed_charge).
+
+    Examples:
+        >>> parse_spec("Fe2+")
+        ('Fe', 2)
+        >>> parse_spec("O2-")
+        ('O', -2)
+
+    """
     ele = re.match(r"[A-Za-z]+", species).group(0)
 
     charge_match = re.search(r"\d+", species)
@@ -22,6 +37,18 @@ def unparse_spec(species: Tuple[str, int]) -> str:
 
     The analogue of :func:`parse_spec`.
 
+    Args:
+        A tuple of (element, signed_charge).
+
+    Returns:
+        String of {element}{absolute_charge}{sign}.
+
+    Examples:
+        >>> unparse_spec(("Fe", 2))
+        'Fe2+'
+        >>> unparse_spec(("O", -2))
+        'O2-'
+
     """
     return f"{species[0]}{abs(species[1])}{get_sign(species[1])}"
 
@@ -30,10 +57,10 @@ def get_sign(charge: int) -> str:
     """Get string representation of a number's sign.
 
     Args:
-        charge: The number whose sign to derive.
+        charge (int): The number whose sign to derive.
 
     Returns:
-        Sign; either '+', '-' or '' for neutral.
+        sign (str): either '+', '-', or '' for neutral.
 
     """
     if charge > 0:
