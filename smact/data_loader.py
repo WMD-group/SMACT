6cce33059e629b1293f77d4804476b09f19c79ac
mts87985 <k.t.butler@bath.ac.uk>
2019-09-25 08:27:05 +0100

    removing gpl headers

--- smact/data_loader.py
+++ smact/data_loader.py
@@ -1,17 +1,3 @@
-###############################################################################
-# Copyright J. M. Skelton, D. W. Davies, A. J. Jackson (2016)                 #
-#                                                                             #
-# This file is part of SMACT: smact.__init__ is free software: you can        #
-# redistribute it and/or modify it under the terms of the GNU General Public  #
-# License as published by the Free Software Foundation, either version 3 of   #
-# the License, or (at your option) any later version.                         #
-# This program is distributed in the hope that it will be useful, but WITHOUT #
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for   #
-# more details.                                                               #
-# You should have received a copy of the GNU General Public License along with#
-# this program.  If not, see <http://www.gnu.org/licenses/>.                  #
-###############################################################################
 
 """
 Provide data from text files while transparently caching for efficiency.
--- smact/lattice.py
+++ smact/lattice.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python
 
-################################################################################
-#  Copyright Keith T. Butler, Adam J. Jackson (2013)                           #
-#                                                                              #
-# This file is part of SMACT: lattice.py is free software: you can             #
-# redistribute it and/or modify it under the terms of the GNU General Public   #
-# License as published by the Free Software Foundation, either version 3 of    #
-# the License, or (at your option) any later version.                          #
-# This program is distributed in the hope that it will be useful, but WITHOUT  #
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        #
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for    #
-# more details.                                                                #
-# You should have received a copy of the GNU General Public License along with #
-# this program.  If not, see <http://www.gnu.org/licenses/>.                   #
-################################################################################
 
 from builtins import object
 import numpy as np
