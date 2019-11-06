3ba4e8d4259fc8f40cb324c8007fee3044f96623
Dan Davies <dandavies99@gmail.com>
2019-11-06 08:22:18 +0000

    update to v2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 __author__ = "Daniel W. Davies"
 __copyright__ = "Copyright Daniel W. Davies, Adam J. Jackson, Keith T. Butler (2019)"
-__version__ = "2.1"
+__version__ = "2.2"
 __maintainer__ = "Daniel W. Davies"
 __email__ = "d.davies16@imperial.ac.uk"
 __date__ = "Jun 10 2019"
@@ -16,14 +16,14 @@ module_dir = os.path.dirname(os.path.abspath(__file__))
 if __name__ == "__main__":
     setup(
       name='SMACT',
-      version='2.1',
+      version='2.2',
       description='Semiconducting Materials by Analogy and Chemical Theory',
       long_description=open(os.path.join(module_dir, 'README.md')).read(),
       long_description_content_type='text/markdown',
       url='https://github.com/WMD-group/SMACT',
       author='Daniel W. Davies',
       author_email='d.davies16@imperial.ac.uk',
-      license='GNU General Public License (GPL) v3',
+      license='MIT',
       packages=['smact', 'smact.tests'],
       package_data={
         'smact': ['data/*.txt', 'data/*.csv', 'data/*.data', 'data/*.xlsx', 'data/*.json']
