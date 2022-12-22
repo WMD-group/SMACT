460c74cbc871a7fd1d88f944c2d54fafdf45e3a4
Anthony <anthony.onwuli16@imperial.ac.uk>
2022-12-22 16:49:01 +0000

    updated docstrings in doper.py

--- smact/dopant_prediction/doper.py
+++ smact/dopant_prediction/doper.py
@@ -6,11 +6,11 @@
 
 # Now 'Doper' can generate possible n-type p-type dopants for multicomponent materials (i.e. Ternary, Quaternary etc).
 # Can plot the result of doping search within a single step
-"""ex) test= Doper(('Cu1+','Zn2+','Ge4+','S2-'))
-         test.get_dopants(num_dopants = 10, plot_heatmap = True)"""
+# """ex) test= Doper(('Cu1+','Zn2+','Ge4+','S2-'))
+#         test.get_dopants(num_dopants = 10, plot_heatmap = True)"""
 
 
-from typing import Tuple
+from typing import List, Tuple
 
 from pymatgen.util import plotting
 
@@ -22,13 +22,30 @@ class Doper:
     """
     A class to search for n & p type dopants
     Methods: get_dopants, plot_dopants
+
+    Attributes:
+        original_species: A tuple which describes the constituent species of a material.
+
+        >>> test= Doper(('Cu1+','Zn2+','Ge4+','S2-'))
+        >>> test.original_species
+            ('Cu1+','Zn2+','Ge4+','S2-')
+
     """
 
     def __init__(self, original_species: Tuple[str, ...]):
 
+        """
+        Intialise the `Doper` class with a tuple of species
+
+        Args:
+            original_species: See :class:`~.Doper`.
+
+        """
         self.original_species = original_species
 
-    def _get_cation_dopants(self, element_objects, cations):
+    def _get_cation_dopants(
+        self, element_objects: List[smact.Element], cations: List[str]
+    ):
         poss_n_type_cat = []
         poss_p_type_cat = []
 
@@ -50,7 +67,9 @@ class Doper:
 
         return poss_n_type_cat, poss_p_type_cat
 
-    def _get_anion_dopants(self, element_objects, anions):
+    def _get_anion_dopants(
+        self, element_objects: List[smact.Element], anions: List[str]
+    ):
         poss_n_type_an = []
         poss_p_type_an = []
 
@@ -70,7 +89,7 @@ class Doper:
                         )
         return poss_n_type_an, poss_p_type_an
 
-    def _plot_dopants(self, results):
+    def _plot_dopants(self, results: dict):
         """
         Uses pymatgen plotting utilities to plot the results of doping search
         """
@@ -85,20 +104,29 @@ class Doper:
 
     def get_dopants(
         self,
-        num_dopants=5,
-        plot_heatmap=False,
+        num_dopants: int = 5,
+        plot_heatmap: bool = False,
     ) -> dict:
         """
         Args:
-            ex) get_dopants(('Ti4+','O2-'))
-
-            original_species (tuple(str)) = ('Cd2+', 'O2-')
             num_dopants (int) = The number of suggestions to return for n- and p-type dopants.
-
+            plot_heatmap(bool) If True, the results of the doping search are plotted as heatmaps
 
         Returns:
             (dict): Dopant suggestions, given as a dictionary with keys
             "n_type_cation", "p_type_cation", "n_type_anion", "p_type_anion".
+
+        Examples:
+            >>> test = Doper(('Ti4+','O2-'))
+            >>> print(test.get_dopants(num_dopants=2))
+                {'n-type cation substitutions': [('Ta5+', 8.790371775858281e-05),
+                ('Nb5+', 7.830035204694342e-05)],
+                'p-type cation substitutions': [('Na1+', 0.00010060400812977031),
+                ('Zn2+', 8.56373996146833e-05)],
+                'n-type anion substitutions': [('F1-', 0.01508116810515677),
+                ('Cl1-', 0.004737202729901607)],
+                'p-type anion substitutions': [('N3-', 0.0014663800608945628),
+                ('C4-', 9.31310255126729e-08)]}
         """
 
         cations = []
