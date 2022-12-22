3a544a47db265711934d37c21fe0fc8fb2bb0773
Anthony <anthony.onwuli16@imperial.ac.uk>
2022-12-22 16:48:31 +0000

    updated smact/dopant_prediction/__init__.py

---
+++ smact/dopant_prediction/__init__.py
@@ -0,0 +1,16 @@
+"""Minimalist dopant prediction tools for materials design."""
+
+import logging
+
+__author__ = "Chloe (Jiwoo) Lee (이지우)"
+__credits__ = {
+    "WMD Group",
+    "Imperial College London",
+    "Anthony Onwuli",
+    "Keith Butler",
+    "Aron Walsh",
+    "Chloe (Jiwoo) Lee (이지우)",
+}
+__status__ = "Development"
+
+logger = logging.getLogger(__name__)
