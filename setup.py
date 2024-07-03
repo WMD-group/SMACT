b947ab22aa4e443b5c7f6dee43791f43feecdb71
Anthony Onwuli <30937913+AntObi@users.noreply.github.com>
2024-07-03 14:24:22 +0100

    Pin numpy<2

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
         test_suite="smact.tests.test",
         install_requires=[
             "scipy",
-            "numpy",
+            "numpy<2",
             "spglib",
             "pymatgen>=2024.2.20",
             "ase",
