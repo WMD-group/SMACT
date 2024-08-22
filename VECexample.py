c84e475ccd346115ca4857c63174d6d71691c9df
ryannduma <ryannduma@gmail.com>
2024-08-22 11:29:03 +0100

    small tweak to csv file naming

--- VECexample.py
+++ VECexample.py
@@ -2,7 +2,7 @@ from smact.properties import valence_electron_count
 
 # Define the compound and the path to the valence data file
 compound = "Ba5In4Bi5"
-valence_file = "element_valence_modifiedry.csv"
+valence_file = "elements_valence_modified.csv"
 
 # Calculate the Valence Electron Count (VEC)
 vec = valence_electron_count(compound, valence_file)
